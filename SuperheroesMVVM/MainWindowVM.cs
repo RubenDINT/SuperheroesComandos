db21cd0f79b03592ab069b6c4e3ea464a6be519c
Ruben9911 <ruben.fortes-marques@iesdoctorbalmis.com>
2022-01-10 14:29:10 +0100

    Agregar archivos de proyecto.

---
+++ SuperheroesMVVM/MainWindowVM.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperheroesMVVM
+{
+    class MainWindowVM : ObservableObject
+    {
+        private List<Superheroe> heroes;
+
+        private Superheroe heroeActual;
+
+        public Superheroe HeroeActual
+        {
+            get { return heroeActual; }
+            set { SetProperty(ref heroeActual, value); }
+        }
+
+        private int total;
+
+        public int Total
+        {
+            get { return total; }
+            set { SetProperty(ref total, value); }
+        }
+
+        private int actual;
+
+        public int Actual
+        {
+            get { return actual; }
+            set { SetProperty(ref actual, value); }
+        }
+
+        public RelayCommand AvanzarCommand{ get; }
+        public RelayCommand RetrocederCommand { get; }
+        public MainWindowVM()
+        {
+            heroes = GetSamplesService.GetSamples();
+            HeroeActual = heroes[0];
+            Total = heroes.Count;
+            Actual = 1;
+            // Instancias de los commandos
+            AvanzarCommand = new RelayCommand(Siguiente);
+            RetrocederCommand = new RelayCommand(Anterior);
+        }
+
+        public void Siguiente()
+        { 
+            if (Actual < Total)
+            {
+                Actual++;
+                HeroeActual = heroes[Actual-1];
+            }
+        }
+
+        public void Anterior()
+        {
+            if (Actual > 1)
+            {
+                Actual--;
+                HeroeActual = heroes[Actual-1];
+            }
+        }
+    }
+}
---
+++ SuperheroesMVVM/Superheroe.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace SuperheroesMVVM
+{
+    class Superheroe : INotifyPropertyChanged
+    {
+        private string _nombre;
+        public string Nombre
+        {
+            get => _nombre;
+            set
+            {
+                if (_nombre != value)
+                {
+                    _nombre = value;
+                    NotifyPropertyChanged("Nombre");
+                }
+            }
+        }
+        private string _imagen;
+        public string Imagen
+        {
+            get => _imagen;
+            set
+            {
+                if (_imagen != value)
+                {
+                    _imagen = value;
+                    NotifyPropertyChanged("Imagen");
+                }
+            }
+        }
+
+        private bool _vengador;
+        public bool Vengador
+        {
+            get => _vengador;
+            set
+            {
+                if (_vengador != value)
+                {
+                    _vengador = value;
+                    NotifyPropertyChanged("Vengador");
+                }
+            }
+        }
+
+        private bool _xmen;
+        public bool Xmen
+        {
+            get => _xmen;
+            set
+            {
+                if (_xmen != value)
+                {
+                    _xmen = value;
+                    NotifyPropertyChanged("Xmen");
+                }
+            }
+        }
+
+        private bool _heroe;
+        public bool Heroe
+        {
+            get => _heroe;
+            set
+            {
+                if (_heroe != value)
+                {
+                    _heroe = value;
+                    NotifyPropertyChanged("Heroe");
+                }
+            }
+        }
+
+        public Superheroe()
+        {
+        }
+
+        public Superheroe(string nombre, string imagen, bool vengador, bool xmen, bool heroe)
+        {
+            Nombre = nombre;
+            Imagen = imagen;
+            Vengador = vengador;
+            Xmen = xmen;
+            Heroe = heroe;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
